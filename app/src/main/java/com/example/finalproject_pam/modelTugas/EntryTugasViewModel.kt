a133c5afaa5a288f6941bdf6f2d0a286b6db5a31
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 12:01:21 +0700

    membuat data class UIStateTugas

--- app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
@@ -1,5 +1,10 @@
 package com.example.finalproject_pam.modelTugas
 
+data class UIStateTugas(
+    val detailTugas: DetailTugas = DetailTugas(),
+    val isEntryValid: Boolean = false
+)
+
 data class DetailTugas(
     val id: Int = 0,
     val mahasiswaId: Int = 0,
