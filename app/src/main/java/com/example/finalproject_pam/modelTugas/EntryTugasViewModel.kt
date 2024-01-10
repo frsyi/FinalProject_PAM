bf2a56298d70613ea54bf7577b139583b8730613
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 12:02:52 +0700

    membuat class EntryTugasViewModel

--- app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
@@ -1,6 +1,32 @@
 package com.example.finalproject_pam.modelTugas
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.finalproject_pam.data.Tugas
+import com.example.finalproject_pam.repository.RepositoryTugas
+
+class EntryTugasViewModel(private val repositoryTugas: RepositoryTugas) : ViewModel() {
+    var uiStateTugas by mutableStateOf(UIStateTugas())
+        private set
+
+    private fun validasiInput(uiState: DetailTugas = uiStateTugas.detailTugas): Boolean {
+        return with(uiState) {
+            namaTugas.isNotBlank() && detailTugas.isNotBlank() && deadline.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailTugas: DetailTugas) {
+        uiStateTugas = UIStateTugas(detailTugas = detailTugas, isEntryValid = validasiInput(detailTugas))
+    }
+
+    suspend fun saveTugas() {
+        if (validasiInput()) {
+            repositoryTugas.insertTugas(uiStateTugas.detailTugas.toTugas())
+        }
+    }
+}
 
 data class UIStateTugas(
     val detailTugas: DetailTugas = DetailTugas(),
