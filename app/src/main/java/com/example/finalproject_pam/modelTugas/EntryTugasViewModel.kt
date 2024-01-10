adf62d99cc0a242ebfef6f1da4e572210140c80c
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 12:00:59 +0700

    membuat data class DetailTugas

---
+++ app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.finalproject_pam.modelTugas
+
+data class DetailTugas(
+    val id: Int = 0,
+    val mahasiswaId: Int = 0,
+    val namaTugas: String = "",
+    val detailTugas: String = "",
+    val deadline: String = ""
+)
