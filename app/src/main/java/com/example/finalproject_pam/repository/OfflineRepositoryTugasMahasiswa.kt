ecf718a2467186b554ea4adf4171ddc064252250
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 09:04:56 +0700

    membuat class OfflineRepositoryTugas

--- app/src/main/java/com/example/finalproject_pam/repository/OfflineRepositoryTugasMahasiswa.kt
+++ app/src/main/java/com/example/finalproject_pam/repository/OfflineRepositoryTugasMahasiswa.kt
@@ -2,6 +2,8 @@ package com.example.finalproject_pam.repository
 
 import com.example.finalproject_pam.data.Mahasiswa
 import com.example.finalproject_pam.data.MahasiswaDao
+import com.example.finalproject_pam.data.Tugas
+import com.example.finalproject_pam.data.TugasDao
 import kotlinx.coroutines.flow.Flow
 
 class OfflineRepositoryMahasiswa(private val mahasiswaDao: MahasiswaDao) : RepositoryMahasiswa {
@@ -18,5 +20,22 @@ class OfflineRepositoryMahasiswa(private val mahasiswaDao: MahasiswaDao) : Repos
 
     override suspend fun updateMahasiswa(mahasiswa: Mahasiswa) {
         mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+}
+
+class OfflineRepositoryTugas(private val tugasDao: TugasDao) : RepositoryTugas {
+
+    override fun getAllTugasStream(mahasiswaId: Int): Flow<List<Tugas>> = tugasDao.getAllTugasByMahasiswaId(mahasiswaId)
+
+    override fun getTugasStream(tugasId: Int): Flow<Tugas?> {
+        return tugasDao.getTugasById(tugasId)
+    }
+
+    override suspend fun insertTugas(tugas: Tugas) = tugasDao.insertTugas(tugas)
+
+    override suspend fun deleteTugas(tugas: Tugas) = tugasDao.deleteTugas(tugas)
+
+    override suspend fun updateTugas(tugas: Tugas) {
+        tugasDao.updateTugas(tugas)
     }
 }
