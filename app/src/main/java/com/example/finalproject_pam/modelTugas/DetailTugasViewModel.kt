7c60cdb094049567bdd388daa244a3bd492a41b6
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 19:16:58 +0700

    membuat class DetailTugasViewModel

--- app/src/main/java/com/example/finalproject_pam/modelTugas/DetailTugasViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelTugas/DetailTugasViewModel.kt
@@ -1,5 +1,22 @@
 package com.example.finalproject_pam.modelTugas
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject_pam.repository.RepositoryTugas
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+
+class DetailTugasViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryTugas: RepositoryTugas
+) : ViewModel() {
+
+    companion object {
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+}
+
 data class ItemDetailTugasUiState(
     val outOfStock: Boolean = true,
     val detailTugas: DetailTugas = DetailTugas()
