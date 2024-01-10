b89b965a1d74b4f323495940e83a52f491019453
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-10 10:41:47 +0700

    menambah class EntryMahasiswaViewModel

--- app/src/main/java/com/example/finalproject_pam/modelMahasiswa/EntryMahasiswaViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelMahasiswa/EntryMahasiswaViewModel.kt
@@ -1,6 +1,35 @@
 package com.example.finalproject_pam.modelMahasiswa
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
 import com.example.finalproject_pam.data.Mahasiswa
+import com.example.finalproject_pam.repository.RepositoryMahasiswa
+
+class EntryMahasiswaViewModel (private val repositoryMahasiswa: RepositoryMahasiswa) : ViewModel() {
+    var uiStateMahasiswa by mutableStateOf(UIStateMahasiswa())
+    private set
+
+            private fun validasiInput(uiState: DetailMahasiswa = uiStateMahasiswa.detailMahasiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && nim.isNotBlank() && semester.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailMahasiswa: DetailMahasiswa) {
+        uiStateMahasiswa = UIStateMahasiswa(
+            detailMahasiswa = detailMahasiswa,
+            isEntryValid = validasiInput(detailMahasiswa)
+        )
+    }
+
+    suspend fun saveMahasiswa() {
+        if (validasiInput()) {
+            repositoryMahasiswa.insertMahasiswa(uiStateMahasiswa.detailMahasiswa.toMahasiswa())
+        }
+    }
+}
 
 data class UIStateMahasiswa(
     val detailMahasiswa: DetailMahasiswa = DetailMahasiswa(),
