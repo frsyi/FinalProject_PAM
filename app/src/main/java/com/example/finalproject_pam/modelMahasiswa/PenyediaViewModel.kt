edd4199b1f9189a9c276081ae3b69415b9f059c3
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-10 11:38:47 +0700

    membuat PenyediaViewModel

---
+++ app/src/main/java/com/example/finalproject_pam/modelMahasiswa/PenyediaViewModel.kt
@@ -0,0 +1,34 @@
+package com.example.finalproject_pam.modelMahasiswa
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.finalproject_pam.AplikasiTugasMahasiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeMahasiswaViewModel(aplikasiTugasMahasiswa().container.repositoryMahasiswa)
+        }
+        initializer {
+            EntryMahasiswaViewModel(aplikasiTugasMahasiswa().container.repositoryMahasiswa)
+        }
+        initializer {
+            DetailsMahasiswaViewModel(
+                createSavedStateHandle(),
+                aplikasiTugasMahasiswa().container.repositoryMahasiswa
+            )
+        }
+        initializer {
+            EditMahasiswaViewModel(
+                createSavedStateHandle(),
+                aplikasiTugasMahasiswa().container.repositoryMahasiswa
+            )
+        }
+    }
+}
+
+fun CreationExtras.aplikasiTugasMahasiswa(): AplikasiTugasMahasiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiTugasMahasiswa)
