b981fc40c5d3ec270a1bc3c4d6f8e94a83c7ce4f
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 19:19:14 +0700

    membuat DestinasiEntryTugas di HalamanEntryTugas

---
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanTugas/HalamanEntryTugas.kt
@@ -0,0 +1,9 @@
+package com.example.finalproject_pam.ui.halamanTugas
+
+import com.example.finalproject_pam.R
+import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+
+object DestinasiEntryTugas: DestinasiNavigasi {
+    override val route = "item_entry_tugas"
+    override val titleRes = R.string.entry_tugas
+}
