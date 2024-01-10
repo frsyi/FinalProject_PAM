1d38a297db5e72ce5691ce41d392999516faef7a
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-10 10:32:32 +0700

    membuat data class DetailMahasiswa

---
+++ app/src/main/java/com/example/finalproject_pam/modelMahasiswa/EntryMahasiswaViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.finalproject_pam.modelMahasiswa
+
+data class DetailMahasiswa(
+    val id: Int = 0,
+    val nama: String = "",
+    val nim: String = "",
+    val semester: String = ""
+)
