137c9b4be02554a0e9a022610067e41152501cf9
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-10 11:00:45 +0700

    membuat class DetailsMahasiswaViewModel

--- app/src/main/java/com/example/finalproject_pam/modelMahasiswa/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelMahasiswa/DetailMahasiswaViewModel.kt
@@ -1,5 +1,28 @@
 package com.example.finalproject_pam.modelMahasiswa
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finalproject_pam.repository.RepositoryMahasiswa
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+
+class DetailsMahasiswaViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMahasiswa: RepositoryMahasiswa
+) : ViewModel() {
+
+
+    companion object{
+        private const val TIMEOUT_MILLIS = 5_000L
+    }
+
+}
+
+
 data class ItemDetailsMahasiswaUiState(
     val outOfStock: Boolean = true,
     val detailMahasiswa: DetailMahasiswa = DetailMahasiswa()
