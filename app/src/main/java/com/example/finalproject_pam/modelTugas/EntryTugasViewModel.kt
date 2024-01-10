99c8cabe405c013ddfcc0a254ab0c3eec22e484a
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-10 12:02:19 +0700

    membuat function di EntryTugasViewModel

--- app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelTugas/EntryTugasViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.finalproject_pam.modelTugas
 
+import com.example.finalproject_pam.data.Tugas
+
 data class UIStateTugas(
     val detailTugas: DetailTugas = DetailTugas(),
     val isEntryValid: Boolean = false
@@ -11,4 +13,25 @@ data class DetailTugas(
     val namaTugas: String = "",
     val detailTugas: String = "",
     val deadline: String = ""
+)
+
+fun DetailTugas.toTugas(): Tugas = Tugas(
+    id = id,
+    mahasiswaId = mahasiswaId,
+    namaTugas = namaTugas,
+    detailTugas = detailTugas,
+    deadline = deadline
+)
+
+fun Tugas.toUiStateTugas(isEntryValid: Boolean = false): UIStateTugas = UIStateTugas(
+    detailTugas = this.toDetailTugas(),
+    isEntryValid = isEntryValid
+)
+
+fun Tugas.toDetailTugas(): DetailTugas = DetailTugas(
+    id = id,
+    mahasiswaId = mahasiswaId,
+    namaTugas = namaTugas,
+    detailTugas = detailTugas,
+    deadline = deadline
 )
