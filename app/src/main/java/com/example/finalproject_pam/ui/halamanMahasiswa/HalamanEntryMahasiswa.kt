39d6ee1d2617c13e6e35feb8e7a7dd7a1097374a
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 21:17:14 +0700

    membuat object DestinasiEntryMahasiswa di HalamanEntryMahasiswa

---
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEntryMahasiswa.kt
@@ -0,0 +1,9 @@
+package com.example.finalproject_pam.ui.halamanMahasiswa
+
+import com.example.finalproject_pam.R
+import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+
+object DestinasiEntryMahasiswa: DestinasiNavigasi {
+    override val route = "item_entry_mahasiswa"
+    override val titleRes = R.string.entry_mahasiswa
+}
