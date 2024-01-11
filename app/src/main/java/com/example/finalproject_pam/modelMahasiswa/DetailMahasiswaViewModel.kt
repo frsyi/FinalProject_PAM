bd66a8a20f906dea8c3c8ce3bb3485d66dcd7c30
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 22:26:55 +0700

    menambahkan class DetailsMahasiswaViewModel dan data class ItemDetailsMahasiswaUiState di DetailsMahasiswaViewModel

--- app/src/main/java/com/example/finalproject_pam/modelMahasiswa/DetailMahasiswaViewModel.kt
+++ app/src/main/java/com/example/finalproject_pam/modelMahasiswa/DetailMahasiswaViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.finalproject_pam.repository.RepositoryMahasiswa
+import com.example.finalproject_pam.ui.halamanMahasiswa.DetailMahasiswaDestination
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNotNull
@@ -14,6 +15,22 @@ class DetailsMahasiswaViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMahasiswa: RepositoryMahasiswa
 ) : ViewModel() {
+    private val mahasiswaId: Int = checkNotNull(savedStateHandle[DetailMahasiswaDestination.mahasiswaIdArg])
+
+    val uiState: StateFlow<ItemDetailsMahasiswaUiState> = repositoryMahasiswa.getMahasiswaStream(mahasiswaId)
+        .filterNotNull()
+        .map {
+            ItemDetailsMahasiswaUiState(detailMahasiswa = it.toDetailMahasiswa())
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+            initialValue = ItemDetailsMahasiswaUiState()
+        )
+
+    suspend fun deleteItem() {
+        repositoryMahasiswa.deleteMahasiswa(uiState.value.detailMahasiswa.toMahasiswa())
+    }
 
     companion object{
         private const val TIMEOUT_MILLIS = 5_000L
