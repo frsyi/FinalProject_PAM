59f93836f46cffbca9215e3e5d9eeea21f96c25e
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 20:34:50 +0700

    menambahkan fuction HalamanHome

---
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanHome.kt
@@ -0,0 +1,16 @@
+package com.example.finalproject_pam.ui.halamanMahasiswa
+
+import androidx.compose.runtime.Composable
+import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+
+object DestinasiHalamanHome: DestinasiNavigasi {
+    override val route = "halaman_home"
+    override val titleRes = 0
+}
+
+@Composable
+fun HalamanHome(
+    onNextButtonClicked: () -> Unit
+){
+
+}
