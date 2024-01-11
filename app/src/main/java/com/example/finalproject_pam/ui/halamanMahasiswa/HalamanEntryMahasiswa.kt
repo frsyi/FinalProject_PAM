90c072520c49f0c023e93441e4bcaaaea1f2edc8
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 21:22:37 +0700

    melengkapi HalamanEntryMahasiswa

--- app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEntryMahasiswa.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEntryMahasiswa.kt
@@ -1,13 +1,36 @@
 package com.example.finalproject_pam.ui.halamanMahasiswa
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject_pam.R
+import com.example.finalproject_pam.modelMahasiswa.DetailMahasiswa
 import com.example.finalproject_pam.modelMahasiswa.EntryMahasiswaViewModel
 import com.example.finalproject_pam.modelMahasiswa.PenyediaViewModel
+import com.example.finalproject_pam.modelMahasiswa.UIStateMahasiswa
 import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+import com.example.finalproject_pam.navigasi.MahasiswaToAppBar
+import kotlinx.coroutines.launch
 
 object DestinasiEntryMahasiswa: DestinasiNavigasi {
     override val route = "item_entry_mahasiswa"
@@ -21,5 +44,108 @@ fun EntryMahasiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EntryMahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold (
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            MahasiswaToAppBar(
+                title = stringResource(DestinasiEntryMahasiswa.titleRes),
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ){innerPadding ->
+        EntryMahasiswaBody(
+            uiStateMahasiswa = viewModel.uiStateMahasiswa,
+            onMahasiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveMahasiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
 
+@Composable
+fun EntryMahasiswaBody(
+    uiStateMahasiswa: UIStateMahasiswa,
+    onMahasiswaValueChange: (DetailMahasiswa) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_Large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        FormInputMahasiswa(
+            detailMahasiswa = uiStateMahasiswa.detailMahasiswa,
+            onValueChange = onMahasiswaValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateMahasiswa.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInputMahasiswa(
+    detailMahasiswa: DetailMahasiswa,
+    modifier: Modifier = Modifier,
+    onValueChange: (DetailMahasiswa) -> Unit = {},
+    enabled: Boolean = true
+){
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ){
+        OutlinedTextField(
+            value = detailMahasiswa.nama,
+            onValueChange = {onValueChange(detailMahasiswa.copy(nama = it))},
+            label = { Text(stringResource(R.string.namaMhs)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailMahasiswa.nim,
+            onValueChange = {onValueChange(detailMahasiswa.copy(nim = it))},
+            label = { Text(stringResource(R.string.nim)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = detailMahasiswa.semester,
+            onValueChange = {onValueChange(detailMahasiswa.copy(semester = it))},
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            label = { Text(stringResource(R.string.semester)) },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled){
+            Text(
+                text = stringResource(R.string.required_field),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+        Divider(
+            thickness = dimensionResource(R.dimen.padding_small),
+            modifier = Modifier.padding(bottom = dimensionResource(R.dimen.padding_medium))
+        )
+    }
 }
