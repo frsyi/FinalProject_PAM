ec9523f47fe5ad78954613cc80b1b042c1e3166d
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 23:33:01 +0700

    memperbarui HalamanHome di halamanMahasiswa

--- app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanHome.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanHome.kt
@@ -1,7 +1,29 @@
-package com.example.finalproject_pam.ui.halamanMahasiswa
-
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.finalproject_pam.R
 import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+import com.example.finalproject_pam.ui.theme.FinalProject_PAMTheme
 
 object DestinasiHalamanHome: DestinasiNavigasi {
     override val route = "halaman_home"
@@ -12,5 +34,61 @@ object DestinasiHalamanHome: DestinasiNavigasi {
 fun HalamanHome(
     onNextButtonClicked: () -> Unit
 ){
+    val image = painterResource(id = R.drawable.tugas)
+    Column (
+        modifier = Modifier,
+        verticalArrangement = Arrangement.SpaceBetween
+    ){
+        OutlinedCard (
+            colors = CardDefaults.cardColors(
+                containerColor = MaterialTheme.colorScheme.surface
+            ),
+            border = BorderStroke(1.dp, Color.Black),
+            modifier = Modifier
+                .fillMaxWidth(0.95f)
+                .padding(vertical = 50.dp)
+                .align(Alignment.CenterHorizontally)
+        ){
+            Column (
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Image(
+                    painter = image,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Pengelola Tugas",
+                    color = Color.DarkGray,
+                    fontFamily = FontFamily.Cursive,
+                    fontSize = 35.sp,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+            }
+        }
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(R.dimen.padding_medium))
+                .weight(1f, false),
+            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.padding_medium)),
+            verticalAlignment = Alignment.Bottom
+        ){
+            Button(
+                modifier = Modifier.weight(1f),
+                onClick = onNextButtonClicked
+            ) {
+                Text(text = "Mulai")
+            }
+        }
+    }
+}
 
+@Preview(showBackground = true)
+@Composable
+fun PreviewHalamanHome() {
+    FinalProject_PAMTheme {
+        HalamanHome(onNextButtonClicked = {})
+    }
 }
