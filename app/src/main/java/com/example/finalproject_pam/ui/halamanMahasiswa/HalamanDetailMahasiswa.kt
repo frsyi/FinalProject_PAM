e98846daea0bed08ce32eb62cc7d8c161db5791c
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 21:41:34 +0700

    menambahkan function DetailMahasiswaScreen di HalamanDetailMahasiswa

--- app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanDetailMahasiswa.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanDetailMahasiswa.kt
@@ -1,6 +1,13 @@
 package com.example.finalproject_pam.ui.halamanMahasiswa
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject_pam.R
+import com.example.finalproject_pam.modelMahasiswa.DetailsMahasiswaViewModel
+import com.example.finalproject_pam.modelMahasiswa.PenyediaViewModel
+import com.example.finalproject_pam.modelTugas.HomeTugasViewModel
 import com.example.finalproject_pam.navigasi.DestinasiNavigasi
 
 object DetailMahasiswaDestination : DestinasiNavigasi {
@@ -8,4 +15,18 @@ object DetailMahasiswaDestination : DestinasiNavigasi {
     override val titleRes = R.string.detail_mahasiswa
     const val mahasiswaIdArg = "itemId"
     val routeWithArgs = "$route/{$mahasiswaIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailMahasiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    navigateToEntryTugas: () -> Unit,
+    onDetailTugasClick: (Int) -> Unit = {},
+    modifier: Modifier = Modifier,
+    viewModel: DetailsMahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    viewModelTugas: HomeTugasViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+
 }
