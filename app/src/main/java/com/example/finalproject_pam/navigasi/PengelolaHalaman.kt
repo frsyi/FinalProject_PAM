e6dafe417a5fcb3e796ee41483bc019a43b3ce47
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-11 20:39:54 +0700

    membuat TugasToAppBar

--- app/src/main/java/com/example/finalproject_pam/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/finalproject_pam/navigasi/PengelolaHalaman.kt
@@ -38,3 +38,29 @@ fun MahasiswaToAppBar(
         }
     )
 }
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TugasToAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack){
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back)
+                    )
+                }
+            }
+        }
+    )
+}
