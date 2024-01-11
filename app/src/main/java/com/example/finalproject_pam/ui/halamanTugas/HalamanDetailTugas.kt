7ccf662634e71b0f2d3d8b0091e84f529e0f5828
fahmarosyidah <fahmarosyi@gmail.com>
2024-01-11 20:32:26 +0700

    membuat DetailTugasScreen

--- app/src/main/java/com/example/finalproject_pam/ui/halamanTugas/HalamanDetailTugas.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanTugas/HalamanDetailTugas.kt
@@ -1,11 +1,200 @@
 package com.example.finalproject_pam.ui.halamanTugas
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject_pam.R
+import com.example.finalproject_pam.data.Tugas
+import com.example.finalproject_pam.modelMahasiswa.PenyediaViewModel
+import com.example.finalproject_pam.modelTugas.DetailTugasViewModel
+import com.example.finalproject_pam.modelTugas.ItemDetailTugasUiState
+import com.example.finalproject_pam.modelTugas.toTugas
 import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+import kotlinx.coroutines.launch
 
 object DetailTugasDestination : DestinasiNavigasi {
     override val route = "item_detail_tugas"
     override val titleRes = R.string.detail_tugas
     const val tugasIdArg = "itemId"
     val routeWithArgs = "$route/{$tugasIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailTugasScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailTugasViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiTugasState.collectAsState()
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navigateToEditItem(uiState.value.detailTugas.id) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_Large))
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_tugas)
+                )
+            }
+        }
+        ,
+        modifier = modifier
+    ) {innerPadding ->
+        ItemDetailsBody(
+            itemDetailTugasUiState = uiState.value,
+            onDelete = {
+                coroutineScope.launch {
+                    viewModel.deleteItem()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
+@Composable
+private fun ItemDetailsBody(
+    itemDetailTugasUiState: ItemDetailTugasUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationrequired by rememberSaveable { mutableStateOf(false) }
+        ItemDetailTugas(
+            tugas = itemDetailTugasUiState.detailTugas.toTugas(), modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(
+            onClick = { deleteConfirmationrequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationrequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationrequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationrequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
+
+@Composable
+fun  ItemDetailTugas(
+    tugas: Tugas, modifier: Modifier = Modifier
+){
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+        ) {
+            ItemDetailsRow(
+                labelResID = R.string.namaTugas,
+                itemDetail = tugas.namaTugas,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemDetailsRow(
+                labelResID = R.string.detail_tugas,
+                itemDetail = tugas.detailTugas,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+            ItemDetailsRow(
+                labelResID = R.string.deadline,
+                itemDetail = tugas.deadline,
+                modifier = Modifier.padding(
+                    horizontal = dimensionResource(id = R.dimen.padding_medium)
+                )
+            )
+        }
+    }
+}
+
+@Composable
+private fun ItemDetailsRow(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+){
+    AlertDialog(
+        onDismissRequest = { /*Do nothing*/ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.delete)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = stringResource(R.string.yes))
+            }
+        },
+    )
 }
