fee63342650d483e826c74f416e3587e3aff8e80
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 21:55:06 +0700

    menambahkan function ItemEditMahasiswaScreen di HalamanEditMahasiswa

--- app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEditMahasiswa.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEditMahasiswa.kt
@@ -1,6 +1,12 @@
 package com.example.finalproject_pam.ui.halamanMahasiswa
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject_pam.R
+import com.example.finalproject_pam.modelMahasiswa.EditMahasiswaViewModel
+import com.example.finalproject_pam.modelMahasiswa.PenyediaViewModel
 import com.example.finalproject_pam.navigasi.DestinasiNavigasi
 
 object ItemEditMahasiswaDestination : DestinasiNavigasi {
@@ -8,4 +14,15 @@ object ItemEditMahasiswaDestination : DestinasiNavigasi {
     override val titleRes = R.string.edit_mahasiswa
     const val itemIdArg = "itemId"
     val routeWithArgs = "$route/{$itemIdArg}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ItemEditMahasiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditMahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
 }
