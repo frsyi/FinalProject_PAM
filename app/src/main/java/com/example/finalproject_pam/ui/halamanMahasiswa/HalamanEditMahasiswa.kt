1481caa5370cd9cfc004932a99f8f76cacc69056
Nur Azizah <nurrrazizahhh11@gmail.com>
2024-01-11 21:58:20 +0700

    menambahkan Scaffold dan EntryMahasiswaBody di HalamanEditMahasiswa

--- app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEditMahasiswa.kt
+++ app/src/main/java/com/example/finalproject_pam/ui/halamanMahasiswa/HalamanEditMahasiswa.kt
@@ -1,13 +1,19 @@
 package com.example.finalproject_pam.ui.halamanMahasiswa
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.finalproject_pam.R
 import com.example.finalproject_pam.modelMahasiswa.EditMahasiswaViewModel
 import com.example.finalproject_pam.modelMahasiswa.PenyediaViewModel
 import com.example.finalproject_pam.navigasi.DestinasiNavigasi
+import com.example.finalproject_pam.navigasi.MahasiswaToAppBar
+import kotlinx.coroutines.launch
 
 object ItemEditMahasiswaDestination : DestinasiNavigasi {
     override val route = "item_edit_mahasiswa"
@@ -24,5 +30,27 @@ fun ItemEditMahasiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EditMahasiswaViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(
+        topBar = {
+            MahasiswaToAppBar(
+                title = stringResource(ItemEditMahasiswaDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        EntryMahasiswaBody(
+            uiStateMahasiswa = viewModel.mahasiswaUiState,
+            onMahasiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateMahasiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
